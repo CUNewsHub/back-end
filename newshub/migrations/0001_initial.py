2a6f7e13ecdf4e30047a1edfb3bff13255c1fa1e
Tamas Sztanka-Toth <sztankatt@gmail.com>
2015-10-16 22:58:26 +0100

    added first version of the schema, and its migration files

---
+++ newshub/migrations/0001_initial.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Article',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=255)),
+                ('headline', models.TextField()),
+                ('content', models.TextField()),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=63)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Profile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('picture', models.ImageField(upload_to=b'profile_pictures/%Y/%m/%d')),
+                ('about', models.TextField()),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=63)),
+                ('article', models.ManyToManyField(to='newshub.Article')),
+                ('category', models.ForeignKey(to='newshub.Category')),
+            ],
+        ),
+    ]
--- newshub/models.py
+++ newshub/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User)
+    picture = models.ImageField(upload_to='profile_pictures/%Y/%m/%d')
+    about = models.TextField()
+
+
+class Article(models.Model):
+    author = models.ForeignKey(User)
+    title = models.CharField(max_length=255)
+    headline = models.TextField()
+    content = models.TextField()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=63)
+
+
+class Tag(models.Model):
+    article = models.ManyToManyField(Article)
+    name = models.CharField(max_length=63)
+    category = models.ForeignKey(Category)
