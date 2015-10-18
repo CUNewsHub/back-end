92ce72bab9d9050841f21e76e7337a3f1d78c50a
Tamas Sztanka-Toth <sztankatt@gmail.com>
2015-10-18 15:27:21 +0100

    adding new entry to secret_data.py

--- src/settings.py
+++ src/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'src.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'newshub',
+        'NAME': secret_data.MYSQL_DB,
         'USER': secret_data.MYSQL_USER,
         'PASSWORD': secret_data.MYSQL_PASSWORD,
         'HOST': 'localhost',
